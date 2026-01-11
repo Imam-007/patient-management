eb7ed01f3328ab7b049613a8e71826d5a47991e3
imam-007 <imimam8409@gmail.com>
2026-01-11 23:51:53 +0530

    Implement get all patients Endpoint

---
+++ patient-service/src/main/java/com/pm/patientservice/service/impl/PatientServiceImpl.java
@@ -0,0 +1,28 @@
+package com.pm.patientservice.service.impl;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.mapper.PatientMapper;
+import com.pm.patientservice.model.Patient;
+import com.pm.patientservice.repository.PatientRepository;
+import com.pm.patientservice.service.PatientService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PatientServiceImpl implements PatientService {
+
+    private final PatientRepository patientRepository;
+
+    public PatientServiceImpl(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients = patientRepository.findAll();
+
+        return patients.stream()
+                .map(PatientMapper::toDto).toList();
+    }
+}
